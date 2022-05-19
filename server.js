41b3a36f917801d514afed0c9dbeb6003b0e16f7
m1052 <105856040+m1052@users.noreply.github.com>
2022-05-19 12:20:50 -0300

    ajeitando arquitetura de pastas

---
+++ server.js
@@ -0,0 +1,16 @@
+const handlebars = require('express-handlebars');
+const express = require('express');
+const app = express();
+const port = 3000;
+//handlebars
+
+app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+//routers
+app.use('/',require('./routes/PostRoutes'));
+//server
+app.listen(port, () =>{
+
+console.log("Rodandado na porta: "+port)
+
+});
