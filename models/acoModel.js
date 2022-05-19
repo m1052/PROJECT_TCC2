5c3b3545502970208b5f0bf45396918c69ecb1be
m1052 <105856040+m1052@users.noreply.github.com>
2022-05-19 17:52:54 -0300

    change to myql connect

--- models/acoModel.js
+++ models/acoModel.js
@@ -1,6 +1,23 @@
 const connect = require('../database/db');
-const aco = connect.connection.query('SELECT * FROM DBTCC.ACS', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
-  })
+class acoModel{
+    _ACODIGO
+    _ANOME
+    _ADESCRICAO
+    constructor (cod,nome,desc){
+        this._ACODIGO = cod;
+        this._ANOME = nome;
+        this._ADESCRICAO = desc;
+    }
 
+}
+
+function getAcoAll(){
+   let row = [];
+    const sqlGet = connect.connection.query('SELECT * FROM DBTCC.ACO',  function(err, rows, fields) {
+    if (err) throw err;
+    //console.log('The solution is: ', rows[0]);
+    return this.rows[0]
+})
+    console.log(rows[0])
+}
+getAcoAll();
